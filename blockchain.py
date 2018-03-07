6a1b2b684041e2b23879c3b70cee894eee9bfffa
guzhenping <598343194@qq.com>
2018-03-07 15:34:43 +0800

    添加路由

--- blockchain.py
+++ blockchain.py
@@ -253,5 +253,44 @@ def full_chain():
     return jsonify(response), 200
 
 
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values = request.get_json()
+    nodes = values.get('nodes')
+    if nodes is None:
+        return 'Error: Please supply a valid list of nodes', 400
+    for node in nodes:
+        blockchain.register_node(node)
+    response = {
+        'message': 'New nodes have been added.',
+        'total_nodes': list(blockchain.nodes)
+    }
+    return jsonify(response), 201
+
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+    if replaced:
+        response = {
+            'message': 'chain is replaced..',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Our chain is the longest..',
+            'chain': blockchain.chain
+        }
+    return response, 200
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    from argparse import ArgumentParser
+
+    # 解析参数
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+    args = parser.parse_args()
+    port = args.port
+
+    app.run(host='0.0.0.0', port=port)
